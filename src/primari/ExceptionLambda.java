55d6398da280b0d952c965f1b8d98facc21d2880
pignak <pignak96@tiscali.it>
2020-03-28 02:58:07 +0100

    uso BiConsumer<Integer,Integer> per fare una somma

--- src/primari/ExceptionLambda.java
+++ src/primari/ExceptionLambda.java
@@ -1,8 +1,19 @@
 package primari;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ExceptionLambda {
+
+    public static void main(String[] args) {
+        final int NUMERO_DA_SOMMARE=10;
+        int [] numeri = { 1,2,3,4 };
+        System.out.println("----QUADRATO DI TUTTI GLI ELEMETNI----");
+        quadrato(numeri, n -> System.out.println(n*n));
+        System.out.println();
+        System.out.println("----SOMMA DI TUTTI GLI ELEMENTI----");
+        somma(numeri,NUMERO_DA_SOMMARE, (a,b) -> System.out.println(a+b));
+    }
 
     public static void quadrato(int[] numeri, Consumer<Integer> consumer){
         for (int x : numeri) {
@@ -10,9 +21,10 @@ public class ExceptionLambda {
         }
     }
 
-    public static void main(String[] args) {
-        int [] numeri = { 1,2,3,4 };
-        quadrato(numeri, n -> System.out.println(n*n));
+    public static void somma(int [] numeri, int ns, BiConsumer<Integer, Integer> operazione){
+        for (int x : numeri) {
+            operazione.accept(x,ns);
+        }
     }
 
 }
