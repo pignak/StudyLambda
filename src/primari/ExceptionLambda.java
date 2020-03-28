d43278bd080cdc714c47fc7b7cb92449e10bf806
pignak <pignak96@tiscali.it>
2020-03-28 03:17:31 +0100

    funzione lambda di funzione lamdba

--- src/primari/ExceptionLambda.java
+++ src/primari/ExceptionLambda.java
@@ -6,31 +6,29 @@ import java.util.function.Consumer;
 public class ExceptionLambda {
 
     public static void main(String[] args) {
-        final int DIVISORE=0;
+        final int NUMERO=0;
         int [] numeri = { 1,2,3,4 };
         System.out.println("----QUADRATO DI TUTTI GLI ELEMETNI----");
         quadrato(numeri, n -> System.out.println(n*n));
         System.out.println();
         System.out.println("----SOMMA DI TUTTI GLI ELEMENTI----");
-        somma(numeri,DIVISORE, (numero,divisore) -> {
-            try{
-                System.out.println(numero/divisore);
-            }catch (Exception e){
-                System.out.println("ERRORE: " + e.getMessage());
-            }
-        });
+        operazioneNumeri(numeri,NUMERO, performOperation((numero, divisore) -> System.out.println("----prima operazione----")));
     }
 
-    public static void quadrato(int[] numeri, Consumer<Integer> consumer){
+    private static void quadrato(int[] numeri, Consumer<Integer> consumer){
         for (int x : numeri) {
                 consumer.accept(x);
         }
     }
 
-    public static void somma(int [] numeri, int ns, BiConsumer<Integer, Integer> operazione){
+    private static void operazioneNumeri(int [] numeri, int ns, BiConsumer<Integer, Integer> operazione){
         for (int x : numeri) {
             operazione.accept(x,ns);
         }
+    }
+
+    private static BiConsumer<Integer,Integer> performOperation(BiConsumer<Integer, Integer> consumer){
+        return (a,b) -> System.out.println("-seconda operazione-");
     }
 
 }
