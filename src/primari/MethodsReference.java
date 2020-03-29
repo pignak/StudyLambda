b6299a5b6fe41028d334563c4b1eae904894bf45
pignak <pignak96@tiscali.it>
2020-03-29 01:41:40 +0100

    uso forEach(lambdaExpression)

--- src/primari/MethodsReference.java
+++ src/primari/MethodsReference.java
@@ -1,16 +1,26 @@
 package primari;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
-public class LambdaAndMethods {
+public class MethodsReference {
 
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> stampaCose());
-        Thread t2 = new Thread(LambdaAndMethods::stampaCose); //MethodReference === () -> stampaCose())
+        Thread t2 = new Thread(MethodsReference::stampaCose); //MethodReference === () -> stampaCose())
         t1.start();
         t2.start();
         System.out.println("----");
         aSchermo(1,System.out::println);
+
+        System.out.println("----");
+        ArrayList<Integer> numeri = new ArrayList<>();
+        numeri.add(10);
+        numeri.add(11);
+        numeri.add(12);
+        numeri.add(13);
+        numeri.forEach(System.out::println);
     }
 
     private static void stampaCose(){
